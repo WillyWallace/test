c4f6e6616fa7e924166dc229cd111228534e3c32
WillyWallace <andreas.foth@uni-leipzig.de>
2019-03-20 10:31:29 +0100

    added test01 and docs

--- code/test01.py
+++ code/test01.py
@@ -1,2 +1,26 @@
+""" Functions for printing the input.
+"""
 x = 5
 print(x)
+
+def peter(x):
+    """High-level API to convert Mira cloud radar Level 1 file into NetCDF file.
+
+    This function converts raw cloud radar file into a much smaller file that
+    contains only the relevant data and can be used in further processing steps.
+
+    Args:
+        mmclx_file (str): Raw radar file in NetCDF format.
+        output_file (str): Output file name.
+        site_properties (dict): Dictionary containing information about the
+            site. Required key value pairs are 'name'.
+        rebin_data (bool, optional): If True, rebins data to 30s resolution.
+            Otherwise keeps the native resolution. Default is False.
+
+    Examples:
+
+    """
+    y = x * 2
+    return y
+
+print(peter(x))
